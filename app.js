e8b1cd43926ab1ddd3189a8f1e99e211a3a9eac8
d-hooper <dallinhoop3@gmail.com>
2025-02-19 14:02:15 -0800

    created draw function for team 1 and for either team based on team id

---
+++ app.js
@@ -0,0 +1,69 @@
+// SECTION STATE
+
+let bank = 100
+
+const players = [
+  { teamNumber: 1, emoji: '🏃‍♂️', skill: 10, name: "D'Marcus Williums" },
+  { teamNumber: 1, emoji: '🤾‍♂️', skill: 30, name: "Tyroil Smoochie-Wallace" },
+  { teamNumber: 1, emoji: '🏇', skill: 88, name: "Jackmerius Tacktheratrix" },
+  { teamNumber: 1, emoji: '🏌️‍♀️', skill: 15, name: "Javaris Jamar Javarison-Lamar" },
+  { teamNumber: 1, emoji: '🏋️‍♂️', skill: 77, name: "D'Pez Poopsie" },
+  { teamNumber: 1, emoji: '🏌️‍♂️', skill: 21, name: "D'Jasper Probincrux III" },
+  { teamNumber: 1, emoji: '🤾', skill: 5, name: "Leoz Maxwell Jilliumz" },
+  { teamNumber: 1, emoji: '🏂', skill: 99, name: "Hingle McCringleberry" },
+  { teamNumber: 1, emoji: '🧘‍♀️', skill: 50, name: "L'Carpetron Dookmarriot" },
+  { teamNumber: 1, emoji: '🚶‍♀️', skill: 1, name: "Xmus Jaxon Flaxon-Waxon" },
+  { teamNumber: 2, emoji: '🏋️‍♀️', skill: 61, name: "Saggitariutt Jefferspin" },
+  { teamNumber: 2, emoji: '🤺', skill: 34, name: "Quatro Quatro" },
+  { teamNumber: 2, emoji: '🏄', skill: 71, name: "X-Wing @Aliciousness" },
+  { teamNumber: 2, emoji: '🧜‍♂️', skill: 76, name: "Bisquiteen Trisket" },
+  { teamNumber: 2, emoji: '🤸', skill: 47, name: "Scoish Velociraptor Maloish" },
+  { teamNumber: 2, emoji: '⛹️‍♀️', skill: 23, name: "Donkey Teeth" },
+  { teamNumber: 2, emoji: '🕴️', skill: 58, name: "T.J. A.J. R.J. Backslashinfourth V" },
+  { teamNumber: 2, emoji: '💃', skill: 99, name: "Firstname Lastname" },
+  { teamNumber: 2, emoji: '🧍‍♂️', skill: 3, name: "Dan Smith" },
+  { teamNumber: 2, emoji: '🐅', skill: 100, name: "Tiger" },
+]
+
+const team1 = []
+
+// !SECTION
+
+// SECTION LOGIC
+
+// !SECTION
+
+// SECTION DRAW / UPDATE
+const drawTeam1 = () => {
+  let emojis = ''
+  for (let i = 0; i < players.length; i++) {
+    const player = players[i];
+    if (player.teamNumber == 1) {
+      team1.push(player);
+      emojis += player.emoji
+    }
+  }
+  team1Elem = document.getElementById('1')
+  team1Elem.innerText = emojis
+
+}
+
+const createTeam = (teamNumber) => {
+  let emojis = ''
+  let currentTeam = [] 
+  for (let i = 0; i < players.length; i++) {
+    const player = players[i];
+    if (player.teamNumber == teamNumber) {
+      currentTeam.push(player)
+      emojis += player.emoji
+    }
+  }
+  teamElem = document.getElementById(teamNumber)
+  teamElem.innerText = emojis
+}
+
+// !SECTION
+
+// SECTION PAGE LOAD
+
+// !SECTION 
